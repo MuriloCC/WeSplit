18f0eb47703eef4d267ba7140f1ea5ab88397d68
MuriloCC <murilo.castilho21@gmail.com>
2022-12-13 19:35:16 -0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Murilo Castilho on 13/12/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
