204d741d8d2d458f4a379fc709fb9ada9fb4f2d3
MuriloCC <murilo.castilho21@gmail.com>
2022-12-13 23:01:26 -0300

    finished

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,19 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	@State private var checkAmount = 0.0
+	@State private var numberOfPeoples = 2
+	@State private var tipPercentage = 20
+	@FocusState private var amountIsFocused: Bool
+	
+	let tipPercentages = [0, 10, 15, 20, 25]
+	
+	var totalPerPerson: Double {
+		let peoplecount = Double(numberOfPeoples + 2)
+		let tipSelection = Double(tipPercentage)
+		
+		let tipValue = checkAmount / 100 * tipSelection
+		let total = checkAmount + tipValue
+		
+		let totalPerPerson = total / peoplecount
+		
+		return totalPerPerson
+	}
+	
+	var body: some View {
+		NavigationView{
+			Form{
+				Section{
+					TextField("Amount: ",
+						value: $checkAmount, format: .currency(
+							code: Locale.current.currencyCode ?? "USD")
+					).keyboardType(.decimalPad)
+						.focused($amountIsFocused)
+					
+					Picker("Number of people", selection: $numberOfPeoples) {
+						ForEach(2..<100) {
+							Text("\($0) people")
+						}
+					}
+				}
+				
+				Section{
+					Picker("Tip percentage", selection: $tipPercentage) {
+						ForEach(tipPercentages, id: \.self) {
+							Text($0, format: .percent)
+						}
+					}.pickerStyle(.segmented)
+				} header: {
+					Text("How much tip do you want to leave?")
+				}
+				
+				Section{
+					Text(totalPerPerson, format: .currency(
+						code: Locale.current.currencyCode ?? "USD"))
+				} header: {
+					Text("Value for each people: ")
+				}
+			}.navigationTitle("WeSplit")
+				.navigationBarTitleDisplayMode(.inline)
+				.toolbar {
+					ToolbarItemGroup(placement: .keyboard) {
+						Spacer()
+						Button("Done") {
+							amountIsFocused = false
+						}
+					}
+				}
+		}
+	}
+}
+
+func pressButton() {
+	print("apertei o botao")
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
